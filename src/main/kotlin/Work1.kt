25f298594cfb1cd7289d0cd89816602efc0ed4bc
kilianAid <kilian_bs@mail.ru>
2022-12-25 15:09:32 +0500

    feat: Add ex.2

--- src/main/kotlin/Work1.kt
+++ src/main/kotlin/Work1.kt
@@ -3,4 +3,32 @@ fun main() {
     val surname = "Корнилов"
     val profession = "Программист"
     println("$name $surname, $profession")
+    val variableByte: Byte = 100
+    val variableShort: Short = 30000
+    val variableInt: Int = 555
+    val variableLong: Long = 64345375368568634
+
+    val variableFloat: Float = 5.63F
+
+    val variableDouble: Double = 7.4324
+
+    val variableStr: String = "<3"
+
+    val variableChar: Char = 'H'
+
+    val variableBooleanTrue: Boolean = true
+    val variableBooleanFalse: Boolean = false
+    val variableNull: Boolean? = null
+
+    println("Byte: $variableByte\n" +
+            "Short: $variableShort\n" +
+            "Int: $variableInt\n" +
+            "Long: $variableLong\n" +
+            "Float: $variableFloat\n" +
+            "Double: $variableDouble\n" +
+            "String: $variableStr\n" +
+            "Char: $variableChar\n" +
+            "Boolean true: $variableBooleanTrue\n" +
+            "Boolean false: $variableBooleanFalse\n" +
+            "Null: $variableNull")
 }
