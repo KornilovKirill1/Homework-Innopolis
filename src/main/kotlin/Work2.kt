c99016f825d7ac7ba037ff68fae8ad018de1dbc7
kilianAid <kilian_bs@mail.ru>
2022-12-29 15:21:48 +0500

    feat: Add ex.2

--- src/main/kotlin/Work2.kt
+++ src/main/kotlin/Work2.kt
@@ -1,5 +1,7 @@
+import java.lang.Math.abs
 fun main() {
     task1()
+    task2()
 }
 fun task1() {
     var inputInfo: String = ""
@@ -11,4 +13,30 @@ fun task1() {
         inputInfo += "\n $word"
     }
     println(inputInfo)
+}
+fun task2() {
+    var sum:Int = 0
+    val number:Int
+
+    print("Напишите число: ")
+
+    try {
+        number = readln().toInt()
+    }
+    catch(e: NumberFormatException) {
+        return println("Для ввода доступно только целое число")
+    }
+
+    for(i in 0..abs(number)){
+        sum += i
+    }
+
+    if(number < 0) sum = -sum
+
+    if(number > 0 ) {
+        println("Сумма от 0 до $number равна $sum")
+    }
+    else {
+        println("Сумма от $number до 0 равна $sum")
+    }
 }
